ebda62db3a1ac245fbec7128c4d8fd972023c0ce
doobidoo <doobidoo@users.noreply.github.com>
2025-09-18 11:00:15 +0900

    chore: bump version to 1.8.2 with enhanced context changelog

--- scripts/build_dxt.py
+++ scripts/build_dxt.py
@@ -112,9 +112,9 @@ class DXTBuilder:
                         existing_data = json.load(f)
                         version = existing_data.get("version", "1.7.0")
                 except:
-                    version = "1.8.1"
+                    version = "1.8.2"
             else:
-                version = "1.8.1"
+                version = "1.8.2"
 
         # Use template if available, otherwise create basic manifest
         if self.manifest_template.exists():
